d1d1d6a898b791d23b3f920006636620afaef6a5
jeroen Van den Broeck <jeroen_vdb1@hotmail.com>
2015-12-18 18:11:15 +0100

    add new routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,10 +15,19 @@ Route::get('/', 'mainController@dashboard');
 
 Route::get('/register', 'UserController@create');
 Route::post('/register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@register']);
+Route::get('/auth/login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@login']);
+Route::get('/logout', ['as' => 'auth.logout', 'uses' => 'Auth\AuthController@logout']);
 
 Route::resource('user', 'UserController');
+Route::get('/art/my-auctions', ['as' => 'myAuctions', 'uses' => 'ArtController@myAuctions']);
 Route::resource('art', 'ArtController');
 Route::resource('artist', 'ArtistController');
 Route::resource('bid', 'BidController');
 Route::resource('picture', 'PictureController');
-Route::resource('watchlist', 'WatchlistController');
+
+// Route::resource('watchlist', 'WatchlistController');
+Route::get('/watchlist', ['as' => 'watchlist.index', 'uses' => 'WatchlistController@index']);
+Route::get('/watchlist/create/{id}', ['as' => 'watchlist.create', 'uses' => 'WatchlistController@create']);
+
+
+Route::get('/FAQ', ['as' => 'FAQ', 'uses' => 'mainController@FAQ']);
