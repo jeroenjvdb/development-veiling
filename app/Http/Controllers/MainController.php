70246d33a5a8e0706833c3e8e13fe184add39c03
jeroen Van den Broeck <jeroen_vdb1@hotmail.com>
2015-12-20 15:22:03 +0100

    add basic functionality

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\FAQ;
+use App\Art;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -23,5 +24,25 @@ class MainController extends Controller
     	$data['FAQ'] = $FAQ;
 
     	return View('FAQ')->with($data);
+    }
+
+    public function search(Request $request)
+    {
+    	$data = [];
+                
+        $search = $request->input('search');
+        if($search == "")
+        {
+            return redirect()->route('home')->withErrors(['please fill in the searchbox']);
+        }
+
+        $data['query'] = $search;
+
+        $artOutput = Art::where('description_nl', 'like', '%' . $search . '%')->get();
+        // var_dump($searchOutput);
+
+    	$data['art'] = $artOutput;
+
+    	return View('Main.search')->with($data);
     }
 }
---
+++ resources/views/Main/search.blade.php
@@ -0,0 +1,10 @@
+@extends('global.master')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<h1>{{ $query }}</h1>
+			
+		</div>
+	</div>
+@stop
