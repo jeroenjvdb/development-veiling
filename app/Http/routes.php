0c5288addb245e495b90a2dcc9cd161922ac4736
jeroen Van den Broeck <jeroen_vdb1@hotmail.com>
2015-12-20 15:22:27 +0100

    modify routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', ['as' => 'home', 'uses' => 'mainController@dashboard']);
+Route::get('/', ['as' => 'home', 'uses' => 'MainController@dashboard']);
+Route::get('/FAQ', ['as' => 'FAQ', 'uses' => 'MainController@FAQ']);
 
 Route::get('/register', ['as' => 'auth.register', 'uses' => 'UserController@create']);
 Route::post('/register', [ 'uses' => 'Auth\AuthController@register']);
@@ -20,8 +21,11 @@ Route::get('/logout', ['as' => 'auth.logout', 'uses' => 'Auth\AuthController@log
 
 Route::get('/profile', ['as' => 'profile', 'uses' => 'UserController@overview']);
 
+
 Route::resource('user', 'UserController');
 Route::get('/art/my-auctions', ['as' => 'myAuctions', 'uses' => 'ArtController@myAuctions']);
+Route::post('/art/search', ['as' => 'art.search', 'uses' => 'ArtController@search']);
+Route::get('/art/{id}/buynow', [ 'as' => 'art.buynow', 'uses' => 'ArtController@buyNow' ]);
 Route::resource('art', 'ArtController');
 Route::resource('artist', 'ArtistController');
 
@@ -33,5 +37,3 @@ Route::resource('picture', 'PictureController');
 Route::get('/watchlist', ['as' => 'watchlist.index', 'uses' => 'WatchlistController@index']);
 Route::get('/watchlist/create/{id}', ['as' => 'watchlist.create', 'uses' => 'WatchlistController@create']);
 
-
-Route::get('/FAQ', ['as' => 'FAQ', 'uses' => 'mainController@FAQ']);
