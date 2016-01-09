e71596cbf0590752bc34129f6941ead294962347
jeroen Van den Broeck <jeroen_vdb1@hotmail.com>
2016-01-09 13:46:59 +0100

    show watchlist

--- resources/views/Watchlist/index.blade.php
+++ resources/views/Watchlist/index.blade.php
@@ -23,7 +23,7 @@
 					</tr>
 					@foreach($watchlist as $auction)
 						<tr>
-							<td>{{-- picture --}}</td>
+							<td><img src="{{ $auction->art->pictures()->first()->url }}" alt=""></td>
 							<td colspan="3">{{ $auction->art->title }}</td>
 							<td>&euro; {{ $auction->art->est_low_price }} - &euro; {{ $auction->art->est_high_price }}</td>
 							<td>{{ $auction->art->end_datetime }}</td>
